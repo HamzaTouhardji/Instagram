bc19fb88a06fc5a39338cde7a7b7a14c138fbece
hamza <h.touhardji>
2019-06-14 18:46:34 +0200

    Bottom Bar Navigation

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.instagram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.instagram.utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: starting.");
+
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationViewEx setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx= (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
     }
 }
---
+++ app/src/main/java/com/example/instagram/utils/BottomNavigationViewHelper.java
@@ -0,0 +1,17 @@
+package com.example.instagram.utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
+        //bottomNavigationViewEx.enableAnimation(false);
+        //bottomNavigationViewEx.enableItemShiftingMode(false);
+        //bottomNavigationViewEx.setTextVisibility(false);
+        //bottomNavigationViewEx.enableShiftingMode(false);
+    }
+}
