2e6eff83bf7f4cf79bef74bee68ce61298f13547
hamza <h.touhardji>
2019-06-21 18:26:41 +0200

    I add camera logo and message fragment + I start a profil layout

--- app/src/main/java/com/example/instagram/Main/MainActivity.java
+++ app/src/main/java/com/example/instagram/Main/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.instagram.Main;
 
 import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,8 +16,8 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int ACTIVITY_NUM = 0;
-    private Context mContext = MainActivity.this;
 
+    private Context mContext = MainActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,28 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: starting.");
 
         setupBottomNavigationView();
+        setupViewPage();
+    }
+
+    /**
+     * Respopnsible for ading the 3 tabs: Camera, Logo and Messages
+     */
+    private void setupViewPage(){
+        SectionsPagesAdapter adapter = new SectionsPagesAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new CameraFragment()); //index 0
+        adapter.addFragment(new HomeFragment()); //index 1
+        adapter.addFragment(new MessagesFragment()); //index 2
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_logo);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_message);
     }
 
     /**
--- app/src/main/java/com/example/instagram/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/instagram/Profile/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,10 +21,30 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started");
 
         setupBottomNavigationView();
+        setupToolBar();
+
+    }
+
+    private void setupToolBar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItem; clicked menu item: " + item);
+                switch (item.getItemId()){
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick: Navigation to Profile Preference.");
+                }
+
+                return false;
+            }
+        });
 
     }
 
@@ -38,5 +59,11 @@ public class ProfileActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
