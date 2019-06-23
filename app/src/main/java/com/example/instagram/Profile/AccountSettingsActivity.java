0aa504b70dbf02ef18a947259d6f4342e4bf4fbb
hamza <h.touhardji>
2019-06-23 11:43:18 +0200

    i add setting activity and complete profile page

---
+++ app/src/main/java/com/example/instagram/Profile/AccountSettingsActivity.java
@@ -0,0 +1,54 @@
+package com.example.instagram.Profile;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.example.instagram.R;
+
+import java.util.ArrayList;
+
+public class AccountSettingsActivity extends AppCompatActivity {
+
+    private static final String TAG = "AccountSettingsActivity";
+    private Context mContext;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accountsettings);
+        mContext = AccountSettingsActivity.this;
+        Log.d(TAG, "onCreate: started.");
+
+        setupSettingsList();
+
+        //setup the backarrow for navigation back to "ProfileActivity"
+        ImageView backArow = (ImageView) findViewById(R.id.backArrow);
+        backArow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigation back to 'ProfileActivity'");
+                finish();
+            }
+        });
+    }
+
+    private void setupSettingsList() {
+        Log.d(TAG, "setupSettingsList: initializing 'Account Settings' List.");
+        ListView listView = (ListView) findViewById(R.id.lvAccountSettings);
+
+        ArrayList<String> options = new ArrayList<>();
+        options.add(getString(R.string.edit_profile));
+        options.add(getString(R.string.sign_out));
+
+        ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
+        listView.setAdapter(adapter);
+
+    }
+}
--- app/src/main/java/com/example/instagram/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/instagram/Profile/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.instagram.Profile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.example.instagram.R;
 import com.example.instagram.utils.BottomNavigationViewHelper;
@@ -16,13 +20,18 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = "ProfileActivity";
     private static final int ACTIVITY_NUM = 4;
+
     private Context mContext = ProfileActivity.this;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started");
+
+        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
+        mProgressBar.setVisibility(View.GONE);
 
         setupBottomNavigationView();
         setupToolBar();
@@ -33,19 +42,15 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
         setSupportActionBar(toolbar);
 
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
+        profileMenu.setOnClickListener(new View.OnClickListener(){
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                Log.d(TAG, "onMenuItem; clicked menu item: " + item);
-                switch (item.getItemId()){
-                    case R.id.profileMenu:
-                        Log.d(TAG, "onMenuItemClick: Navigation to Profile Preference.");
-                }
-
-                return false;
+            public void onClick(View v){
+                Log.d(TAG, "onClick: navigating to account settings.");
+                Intent intent = new Intent (mContext, AccountSettingsActivity.class);
+                startActivity(intent);
             }
         });
-
     }
 
     /**
@@ -61,9 +66,4 @@ public class ProfileActivity extends AppCompatActivity {
         menuItem.setChecked(true);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.profile_menu, menu);
-        return true;
-    }
 }
