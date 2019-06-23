fc33004432f9cf0da6b56a78757348513bfe37a8
hamza <h.touhardji>
2019-06-23 19:41:43 +0200

    edit profile but there is a problem

--- app/src/main/java/com/example/instagram/Profile/EditProfileFragment.java
+++ app/src/main/java/com/example/instagram/Profile/EditProfileFragment.java
@@ -4,19 +4,41 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.instagram.R;
+import com.example.instagram.utils.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class EditProfileFragment extends Fragment {
     private static final String TAG = "EditProfileFragment";
+    private ImageView mProfilePhoto;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+        setProfileImage();
+
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage(){
+        Log.d(TAG, "setProfileImage: setting profile image.");
+        String imgURL = "www.google.com/search?q=image&client=ubuntu&hs=zwT&channel=fs&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjZ4-y1joDjAhUKQ0EAHb7zA1YQ_AUIECgB&biw=1541&bih=779#imgrc=CJwabmfmxl2ySM:";
+        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "https://");
+
     }
 }
